4c1f7b575316007692c2f63c41978794a0880bcb
Harshad <naikharshad01@gmail.com>
2020-04-07 21:10:37 +0530

    Updates

---
+++ precache-manifest.e9e9c0a99d8dfb94f05571b0176b634f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d7b3f5edc63d3f222dc286d7e4fddc",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "9832c9341040566ac01d",
+    "url": "/meme-generator/static/css/main.ec773f69.chunk.css"
+  },
+  {
+    "revision": "4dd7628b7e88f8592548",
+    "url": "/meme-generator/static/js/2.4d41db06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.4d41db06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9832c9341040566ac01d",
+    "url": "/meme-generator/static/js/main.439c535b.chunk.js"
+  },
+  {
+    "revision": "f938386f2eec8fc03e06",
+    "url": "/meme-generator/static/js/runtime-main.a330aea1.js"
+  }
+]);
